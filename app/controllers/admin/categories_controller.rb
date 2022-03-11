cb856b593ddfc5cb894ab80900d7498003685987
Stas74 <splotnikov74@gmail.com>
2022-03-10 19:48:15 -0700

    Add admin categories function

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,11 +1,31 @@
 class Admin::CategoriesController < ApplicationController
-  def index    
+  http_basic_authenticate_with name: ENV['username'], password: ENV['password']
+
+  def index
+    @categories = Category.order(name: :asc).all 
   end
 
-  def new    
+  def new
+    @category = Category.new
   end
 
   def create    
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
+
   
+  private
+
+    def category_params
+      params.require(:category).permit(
+        :name
+      )
+    end
+
 end
