88f64bbff66d9f255313d12379b7718a1b58d150
Stas74 <splotnikov74@gmail.com>
2022-03-10 20:39:48 -0700

    Setup user model. Migrate user db.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   resources :categories, only: [:show]
   get '/about', to:'about#index'
 
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
